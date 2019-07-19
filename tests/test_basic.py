3457c4dffaec8317bbae504eb804b26beffe54c0
Mustafa Akçakaya <mgsmus@gmail.com>
2019-07-19 06:31:08 +0300

    Added some tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 import os
 
@@ -11,4 +12,17 @@ class FavigenTestCase(unittest.TestCase):
         favigen.make_dirs()
         self.assertTrue(os.path.exists(favigen.tmp_path and favigen.output_path))
 
+    def test_argparser(self):
+        favigen = Favigen()
+        arg_name = 'logo.png'
+        sys.argv[1:] = [arg_name]
+        self.assertEqual(arg_name, favigen.parser.parse_args().file)
 
+    def test_extras(self):
+        favigen = Favigen()
+        favigen.make_dirs()
+        favigen.create_extras()
+        b = os.path.join(favigen.tmp_path, 'browserconfig.xml')
+        m = os.path.join(favigen.tmp_path, 'manifest.json')
+        h = os.path.join(favigen.tmp_path, 'html.txt')
+        self.assertTrue(os.path.exists(b and m and h))
